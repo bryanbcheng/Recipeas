9fb93ff7a0025cca4405339af1274c630e8f3340
Bryan Cheng <bryan.b.cheng@gmail.com>
2015-03-21 16:28:23 -0700

    Initial Commit

---
+++ Recipeas/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Recipeas
+//
+//  Created by Bryan Cheng on 3/21/15.
+//  Copyright (c) 2015 Bryan Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Recipeas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Recipeas
+//
+//  Created by Bryan Cheng on 3/21/15.
+//  Copyright (c) 2015 Bryan Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
